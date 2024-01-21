76a01640728f1d39356c1336bfb825299d6178ac
FloresI1 <shusha_03@mail.ru>
2024-01-21 18:51:22 +0300

    add .env

--- main.go
+++ main.go
@@ -11,17 +11,42 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 const port = ":8080"
 
 func main() {
+	// Загрузка переменных окружения из файла .env
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	// Получение значения переменной окружения GIN_MODE
+	ginMode := os.Getenv("GIN_MODE")
+
+	// Установка режима работы Gin в соответствии с переменной окружения
+	switch ginMode {
+	case "release":
+		gin.SetMode(gin.ReleaseMode)
+	case "debug":
+		gin.SetMode(gin.DebugMode)
+	case "test":
+		gin.SetMode(gin.TestMode)
+	default:
+		log.Fatal("Invalid GIN_MODE. Use 'release', 'debug', or 'test'")
+	}
+
 	// Инициализация базы данных
 	db, err := database.InitDB()
 	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
+		log.Println("Failed to initialize database:", err)
+		return
 	}
+
 	defer db.Close()
+
+	log.Println("Database initialized successfully")
 
 	// Инициализация Gin
 	r := gin.Default()
@@ -35,10 +60,12 @@ func main() {
 		Handler: r,
 	}
 
+	log.Printf("Server is starting and listening on %s...\n", port)
+
 	// Запуск сервера в отдельной goroutine
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Error starting server: %v", err)
+			log.Println("Error starting server:", err)
 		}
 	}()
 
@@ -56,8 +83,8 @@ func main() {
 
 	// Закрываем сервер
 	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Server shutdown error: %v", err)
+		log.Println("Server shutdown error:", err)
+	} else {
+		log.Println("Server gracefully stopped")
 	}
-
-	log.Println("Server gracefully stopped")
 }

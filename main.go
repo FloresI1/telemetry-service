3be75acd33d8b7ba75e7252f0422232af803b36f
FloresI1 <shusha_03@mail.ru>
2024-01-21 13:27:10 +0300

    add correct Shutdown

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"forinter/database"
 	"forinter/handler"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,9 +29,35 @@ func main() {
 	// Обработчик для маршрута /track
 	r.POST("/track", handler.TrackHandler(db))
 
-	// Запуск сервера
-	log.Printf("Server is running on http://localhost%s\n", port)
-	if err := r.Run(port); err != nil {
-		log.Fatalf("Error starting server: %v", err)
+	// Создание http.Server для Gin
+	server := &http.Server{
+		Addr:    port,
+		Handler: r,
 	}
+
+	// Запуск сервера в отдельной goroutine
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	// Ожидание сигнала завершения работы программы
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop // Ждем сигнала завершения
+
+	log.Println("Shutting down server...")
+
+	// Создаем контекст с таймаутом для завершения работы сервера
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Закрываем сервер
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown error: %v", err)
+	}
+
+	log.Println("Server gracefully stopped")
 }
